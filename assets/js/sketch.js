50dbaa0fc74d51664384b6069273c0053f5ece20
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-26 23:56:53 +0200

    Added motion helper js

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -4,10 +4,11 @@ function setup() {
 	new Canvas(1080, 1920);
 	world.gravity.y = 10;
 	ball = new Sprite(40, 30, 50);
-	firstFloor = new Sprite(40, 155, 80, 5, 'static');
-	secondFloor = new Sprite(40, 155, 80, 5, 'static');
+	firstFloor = new Sprite(30, 50, [[60, 20], [90, -9], [90, 9]], 's');
+	secondFloor = new Sprite(250, 155, 500, 5, 'static');
 }
 
 function draw() {
 	clear();
+	ball.moveTowards(mouse);
 }
